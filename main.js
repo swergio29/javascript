d4bafd16b101e18b682ac8719017aeb7245bc8c7
swergio29 <84935678+swergio29@users.noreply.github.com>
2021-08-29 18:31:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+// darkMode
 $('.checkbox').click(function(){
     if( $('input.checkbox').is(':checked')){
-        $('theme').attr('href', 'dark.css');
+        $('.theme').attr('href', 'dark.css');
     }else{
         $('.theme').attr('href', 'light.css');
     }
 });
+
+
+// NavBar
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".bx-search");
+
+btn.onclick = function() {
+    sidebar.classList.toggle("active");
+}
+searchBtn.onclick = function() {
+    sidebar.classList.toggle("active");
+}
+
+searchBtn.onclick = function() {
+    sidebar.classList.toggle("active");
+}
